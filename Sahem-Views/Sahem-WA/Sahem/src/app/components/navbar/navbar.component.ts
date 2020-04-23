e8ccdc2cc536b546b73dcbfe8ff8699f5af0fb7a
Taha Bouhsine <45195299+skywolfmo@users.noreply.github.com>
2020-04-23 00:25:38 +0000

    nav bar kinda done with needs the logic

--- Sahem-Views/Sahem-WA/Sahem/src/app/components/navbar/navbar.component.ts
+++ Sahem-Views/Sahem-WA/Sahem/src/app/components/navbar/navbar.component.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  theme: any;
+  bodyClass: any;
+  themeMap = {
+    dark: "light",
+    light: "solar",
+    solar: "dark"
+  };
+  tmp: any;
   constructor() { }
 
   ngOnInit(): void {
+    const theme = localStorage.getItem('theme') || (this.tmp = Object.keys(this.themeMap)[0], localStorage.setItem('theme', this.tmp), this.tmp);
+    const bodyClass = document.body.classList;
+    bodyClass.add(theme);
+
+  }
+
+
+
+
+  toggleTheme() {
+    const current = localStorage.getItem('theme');
+    const next = this.themeMap[current];
+
+    this.bodyClass.replace(current, next);
+    localStorage.setItem('theme', next);
+    console.log("clicked");
   }
 
 }
